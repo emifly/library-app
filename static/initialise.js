d8e6950213449e2a369e55a7d6333f083aab4747
Richard Ladley <Richard.Ladley@softwire.com>
2019-03-26 11:02:35 +0000

    Moved js to a separate file

---
+++ static/initialise.js
@@ -0,0 +1,56 @@
+(function() {
+    // Set up slick carousel
+    $('.news-slider').slick({
+        dots: true,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 560,
+            settings: {
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }]
+    });
+
+    // Navbar smoothly change properties
+    window.onscroll = function() {
+        var topnav = document.getElementById('top-nav');
+        var topbtn = document.getElementById('nav-btn');
+        var signoutbtn = document.getElementById('signout-btn');
+        if (window.pageYOffset < 10) {
+            if (topnav.classList.contains('blue-white-nav')) {
+                topnav.classList.add('transparent-nav');
+                topnav.classList.remove('blue-white-nav');
+                topbtn.classList.add('btn-outline-info');
+                topbtn.classList.remove('btn-info');
+                signoutbtn.classList.add('btn-outline-info');
+                signoutbtn.classList.remove('btn-info');
+            }
+        } else if (topnav.classList.contains('transparent-nav')) {
+            topnav.classList.add('blue-white-nav');
+            topnav.classList.remove('transparent-nav');
+            topbtn.classList.add('btn-info');
+            topbtn.classList.remove('btn-outline-info');
+            signoutbtn.classList.add('btn-info');
+            signoutbtn.classList.remove('btn-outline-info');
+        }
+    }
+
+    $('.edit').on('click', function() {
+        // Add form-control
+        // Remove form-control-plaintext
+        // Remove readonly
+        // Make hidden buttons visible
+        // Make edit button (self) invisible
+    });
+})();
