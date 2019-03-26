46ccbcb53e37dcebacc429e6feb50b8086b4beb6
Richard Ladley <Richard.Ladley@softwire.com>
2019-03-26 14:56:18 +0000

    Added Loan table to migration

--- migrations/20190326135300_loan_tables.py
+++ migrations/20190326135300_loan_tables.py
@@ -1,12 +1,24 @@
 def upgrade(conn):
     conn.execute("DROP TABLE PastLoan;")
-    print("bbb")
+
+    conn.execute('''CREATE TABLE IF NOT EXISTS Loan (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        borrowerId INTEGER NOT NULL,
+        hardCopyId INTEGER NOT NULL,
+
+        borrowDate INTEGER NOT NULL,
+        dueDate INTEGER NOT NULL,
+        returnDate INTEGER,
+            FOREIGN KEY (borrowerId) REFERENCES PublicUser(id),
+            FOREIGN KEY (hardCopyId) REFERENCES HardCopy(id)
+    );''')
 
     conn.commit()
 
-    
 
 def downgrade(conn):
+    conn.execute("DROP TABLE Loan;")
+
     conn.execute('''CREATE TABLE IF NOT EXISTS PastLoan (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         userId INTEGER NOT NULL,
@@ -17,6 +29,5 @@ def downgrade(conn):
             FOREIGN KEY (userId) REFERENCES PublicUser(id),
             FOREIGN KEY (bookId) REFERENCES HardCopy(id)
     );''')
-
 
     conn.commit()
