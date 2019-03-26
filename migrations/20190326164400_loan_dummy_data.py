796bae853f08a26625f615bb3e57899d9a4afefc
Richard Ladley <Richard.Ladley@softwire.com>
2019-03-26 16:47:06 +0000

    Migrations to remove OnlineResource table and add dummy online resources

---
+++ migrations/20190326164400_loan_dummy_data.py
@@ -0,0 +1,15 @@
+def upgrade(conn):
+    # We are no longer going to use OnlineResource
+    conn.execute('DROP TABLE OnlineResource;')
+
+    # Add some dummy onine resources
+    conn.execute('ALTER TABLE BookDetail ADD url TEXT;')
+    conn.execute('UPDATE BookDetail SET url = "https://en.wikipedia.org/wiki/Harry_Potter_and_the_Philosopher%27s_Stone" WHERE id = 1;')
+    conn.execute('UPDATE BookDetail SET url = "https://en.wikipedia.org/wiki/Harry_Potter_and_the_Chamber_of_Secrets" WHERE id = 2;')
+    conn.execute('UPDATE BookDetail SET url = "https://en.wikipedia.org/wiki/Harry_Potter_and_the_Prisoner_of_Azkaban" WHERE id = 3;')
+
+    conn.commit()
+
+
+def downgrade(conn):
+    print("Sorry downgrade not implmented")
